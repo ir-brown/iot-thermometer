f9e462244930c211b9cbaf03b2052ee511befd2f
Ian <ianworb@gmail.com>
2019-06-17 18:11:40 -0700

    Implemented DynamoDB table scan endpoint to fetch current temperature

--- thermo_app.py
+++ thermo_app.py
@@ -1,9 +1,44 @@
 from flask import Flask
-import thermometer
+from flask import jsonify
+from boto3.dynamodb.conditions import Key, Attr
+import boto3
+import datetime
+import decimal
+
+#TODO: Return latest temperature reading when two items are returned from DB
+#TODO: Decide on a response format
 
 app = Flask(__name__)
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Temperature')
 
-@app.route("/read")
-def checkTemp():
-    temp = thermometer.read_temp()
-    return 'Temperature: ' + str(temp) + 'F'
+@app.route("/current")
+def fetchCurrentTemp():
+    currentTime = datetime.datetime.now()
+    window = currentTime - datetime.timedelta(minutes=15) # 15 minutes ago
+
+    response = table.scan(
+        FilterExpression=Attr('Datetime')
+        .between(window.strftime("%Y-%m-%d %H:%M"), currentTime.strftime("%Y-%m-%d %H:%M"))
+    )
+    response = replace_decimals(response)
+
+    return jsonify(response['Items'])
+
+# Stolen with love from garnaat
+def replace_decimals(obj):
+    if isinstance(obj, list):
+        for i in range(len(obj)):
+            obj[i] = replace_decimals(obj[i])
+        return obj
+    elif isinstance(obj, dict):
+        for k in obj.keys():
+            obj[k] = replace_decimals(obj[k])
+        return obj
+    elif isinstance(obj, decimal.Decimal):
+        if obj % 1 == 0:
+            return int(obj)
+        else:
+            return float(obj)
+    else:
+        return obj
